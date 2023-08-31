87dfe03d255a05f6514bef43aa52abfb8bdb9a99
SafarovHusan200 <safarovhusan2000@gmail.com>
2023-08-31 17:08:34 +0500

    home page complate

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,16 @@
+import BarchartBox from "../../components/barChartBox/BarChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
+import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
 
 export default function Home() {
@@ -7,14 +19,30 @@ export default function Home() {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">box2</div>
-      <div className="box box3">box3</div>
-      <div className="box box4">box4</div>
-      <div className="box box5">box5</div>
-      <div className="box box6">box6</div>
-      <div className="box box7">box7</div>
-      <div className="box box8">box8</div>
-      <div className="box box9">box9</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      <div className="box box8">
+        <BarchartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarchartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 }
